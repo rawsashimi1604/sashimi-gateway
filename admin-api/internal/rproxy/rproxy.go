90ec361e58ec9db54e0742077c5b07cd81a890bd
rawsashimi1604 <looweiren@gmail.com>
2023-08-20 13:07:02 +0800

    created functional reverse proxy

--- admin-api/internal/rproxy/rproxy.go
+++ admin-api/internal/rproxy/rproxy.go
@@ -1,8 +1,9 @@
 package rproxy
 
 import (
-	"encoding/json"
+	"io"
 	"net/http"
+	"net/url"
 	"time"
 
 	"github.com/rs/zerolog/log"
@@ -16,18 +17,35 @@ type Response struct {
 
 func ForwardRequest(w http.ResponseWriter, req *http.Request) {
 	log.Info().Msg("Reverse proxy received request: " + req.Host)
-
-	// Forward the request
 	log.Info().Msg(time.Now().String())
-	log.Info().Msg(req.Host)
-	log.Info().Msg(req.URL.Host)
-	log.Info().Msg(req.URL.Scheme)
-	log.Info().Msg(req.RequestURI)
 
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(Response{
-		Code:       http.StatusOK,
-		ReceivedAt: time.Now(),
-		Data:       "Hello world",
-	})
+	// Define service server
+	serviceURL, err := url.Parse("http://localhost:8081")
+	if err != nil {
+		log.Fatal().Msg("invalid url passed in.")
+	}
+
+	// We can't have this set when using http.DefaultClient
+	req.Host = serviceURL.Host
+	req.URL.Host = serviceURL.Host
+	req.URL.Scheme = serviceURL.Scheme
+	req.RequestURI = ""
+
+	serviceResponse, err := http.DefaultClient.Do(req)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Info().Msg(err.Error())
+		log.Info().Msg("something went wrong when forwarding the request")
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	io.Copy(w, serviceResponse.Body)
+
+	// w.Header().Set("Content-Type", "application/json")
+	// json.NewEncoder(w).Encode(Response{
+	// 	Code:       http.StatusOK,
+	// 	ReceivedAt: time.Now(),
+	// 	Data:       "Hello world",
+	// })
 }
