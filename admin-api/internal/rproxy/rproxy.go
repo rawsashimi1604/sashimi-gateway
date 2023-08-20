3faf56578a5dc45b6dec6f2c139ef387faa30fea
rawsashimi1604 <looweiren@gmail.com>
2023-08-20 13:29:01 +0800

    added conditional log message

--- admin-api/internal/rproxy/rproxy.go
+++ admin-api/internal/rproxy/rproxy.go
@@ -1,7 +1,7 @@
 package rproxy
 
 import (
-	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"net/url"
@@ -14,6 +14,20 @@ func ForwardRequest(w http.ResponseWriter, req *http.Request) {
 	log.Info().Msg("Reverse proxy received request: " + req.Host)
 	log.Info().Msg(time.Now().String())
 
+	// Define service server
+	serviceURL, err := url.Parse("http://localhost:8081")
+	if err != nil {
+		log.Fatal().Msg("invalid url passed in.")
+	}
+
+	req.Host = serviceURL.Host
+	req.URL.Host = serviceURL.Host
+	req.URL.Scheme = serviceURL.Scheme
+	log.Info().Msg("host: " + serviceURL.Host)
+	log.Info().Msg("scheme: " + serviceURL.Scheme)
+	// We can't have this set when using http.DefaultClient
+	req.RequestURI = ""
+
 	// Read request body
 	reqBodyBytes, err := io.ReadAll(req.Body)
 	if err != nil {
@@ -21,21 +35,14 @@ func ForwardRequest(w http.ResponseWriter, req *http.Request) {
 		http.Error(w, "Failed to read request body", http.StatusInternalServerError)
 		return
 	}
-	// You might want to log the request body here
-	log.Info().Msg("body: " + string(reqBodyBytes))
+	log.Info().Msg(fmt.Sprintf("request body: %q", func() string {
+		if len(reqBodyBytes) == 0 {
+			return "No body found."
+		}
+		return string(reqBodyBytes)
+	}()))
 
-	// Define service server
-	serviceURL, err := url.Parse("http://localhost:8081")
-	if err != nil {
-		log.Fatal().Msg("invalid url passed in.")
-	}
-
-	// We can't have this set when using http.DefaultClient
-	req.Host = serviceURL.Host
-	req.URL.Host = serviceURL.Host
-	req.URL.Scheme = serviceURL.Scheme
-	req.RequestURI = ""
-
+	// Send Http Request to the service
 	serviceResponse, err := http.DefaultClient.Do(req)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
@@ -44,12 +51,17 @@ func ForwardRequest(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	reqBody, err := json.Marshal(serviceResponse.Body)
+	// Read the response body
+	respBodyBytes, err := io.ReadAll(serviceResponse.Body)
 	if err != nil {
-		log.Info().Msg("something went wrong when marshalling json.")
+		log.Info().Msg("Failed to read service response body")
+		http.Error(w, "Failed to read service response", http.StatusInternalServerError)
+		return
 	}
+	log.Info().Msg("response body: " + string(respBodyBytes))
+
+	// Send back response
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
-	w.Write(reqBody)
-	// io.Copy(w, serviceResponse.Body)
+	w.Write(respBodyBytes)
 }
