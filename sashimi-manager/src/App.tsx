6ba1d6a146617221eb9d46be46f600b4f3f4ebcd
rawsashimi1604 <looweiren@gmail.com>
2023-08-23 19:13:13 +0800

    added prettier and other dev tools

--- sashimi-manager/src/App.tsx
+++ sashimi-manager/src/App.tsx
@@ -1,21 +1,27 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import Footer from './components/layout/Footer';
+import Navbar from './components/layout/Navbar';
 
 function App() {
-
   const router = createBrowserRouter([
     {
-      path: "/",
-      element: <div>Hello world!</div>,
+      path: '/',
+      element: <div>Hello world!</div>
     },
     {
-      path: "/about",
-      element: <div>About</div>,
-    },
+      path: '/about',
+      element: <div>About</div>
+    }
   ]);
 
   return (
-    <RouterProvider router={router} />
-  )
+    <>
+      <Navbar />
+      <RouterProvider router={router} />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
