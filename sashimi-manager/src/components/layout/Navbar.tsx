b0f44dba990f7cdfb5621dd3e5ce81c71398e759
rawsashimi1604 <looweiren@gmail.com>
2023-08-28 17:10:53 +0800

    added breadcrumb

--- sashimi-manager/src/components/layout/Navbar.tsx
+++ sashimi-manager/src/components/layout/Navbar.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import Breadcrumbs from '../typography/Breadcrumbs';
+
 function Navbar() {
   return (
     <nav>
+      <Breadcrumbs />
       Navbar
       <span>hello world</span>
     </nav>
--- sashimi-manager/src/components/typography/Breadcrumbs.tsx
+++ sashimi-manager/src/components/typography/Breadcrumbs.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+import useBreadcrumbs from 'use-react-router-breadcrumbs';
 
 function Breadcrumbs() {
-  return <div>Breadcrumbs</div>;
+  const location = useLocation();
+  const breadcrumbs = useBreadcrumbs();
+  console.log(breadcrumbs);
+
+  return (
+    <div className="flex flex-row items-center text-sm gap-1">
+      <span className="text-gray-500">manager {'>'}</span>
+      {breadcrumbs.map((breadcrumb) => {
+        return (
+          <span
+            className={`${
+              breadcrumb.key === location.pathname
+                ? 'font-semibold'
+                : 'text-gray-500'
+            }`}
+          >
+            {location.pathname === '/' ? 'home' : breadcrumb.key}
+          </span>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Breadcrumbs;
