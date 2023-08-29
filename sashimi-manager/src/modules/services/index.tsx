affac1e03aec08c8a0b4f0c81d39fe1c9eb83cfd
rawsashimi1604 <looweiren@gmail.com>
2023-08-29 11:08:16 +0800

    added add service btn

--- sashimi-manager/src/modules/services/index.tsx
+++ sashimi-manager/src/modules/services/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { GrServices } from 'react-icons/gr';
 
 import Container from '../../components/layout/Container';
 import Header from '../../components/typography/Header';
@@ -8,7 +9,13 @@ function Services() {
   return (
     <Container>
       <Header text="gateway services" align="left" size="sm" />
-      <h3 className="text-xs -mt-2 text-gray-600 mb-4">gateway services</h3>
+
+      <div className="flex flex-row items-center justify-between mb-3">
+        <h3 className="text-xs -mt-2 text-gray-600 ">gateway services</h3>
+        <button className="flex-end text-xs py-1 px-2 bg-blue-500 text-white shadow-md rounded-lg font-sans border-0 duration-300 transition-all hover:-translate-y-1 hover:shadow-lg">
+          <span>add service</span>
+        </button>
+      </div>
 
       <Table />
     </Container>
