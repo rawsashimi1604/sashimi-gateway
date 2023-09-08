b686086328eb833812c2f8bb58acfc3530617ae7
rawsashimi1604 <looweiren@gmail.com>
2023-09-08 19:27:01 +0800

    cleaned up server code for websockets, stopped sending mock requests

--- admin-api/internal/jobs/requests.go
+++ admin-api/internal/jobs/requests.go
@@ -3,9 +3,7 @@ package jobs
 import (
 	"time"
 
-	"github.com/google/uuid"
 	"github.com/rawsashimi1604/sashimi-gateway/admin-api/internal/api/analytics"
-	"github.com/rawsashimi1604/sashimi-gateway/admin-api/internal/models"
 	"github.com/rawsashimi1604/sashimi-gateway/admin-api/internal/websocket"
 	"github.com/robfig/cron/v3"
 )
@@ -36,19 +34,6 @@ func (rcj *RequestCronJob) Start() {
 
 func (rcj *RequestCronJob) run() {
 	requests := rcj.AnalyticsTracker.StoreRequests()
-	requests = append(
-		requests,
-		models.ApiRequest{
-			Id:        uuid.New(),
-			ServiceId: 1,
-			RouteId:   1,
-			Path:      "/test",
-			Method:    "GET",
-			Time:      time.Now(),
-			Code:      200,
-		},
-	)
-	rcj.WebsocketServer.BroadcastMessage("request cron job!!!")
 	if len(requests) > 0 {
 		rcj.WebsocketServer.BroadcastRequests(requests)
 	}
