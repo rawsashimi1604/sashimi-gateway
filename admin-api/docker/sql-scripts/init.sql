ccf84274965a68f5287e7271efaa67f4ad136f82
rawsashimi1604 <looweiren@gmail.com>
2023-09-23 19:28:19 +0800

    fix docker init

--- admin-api/docker/sql-scripts/init.sql
+++ admin-api/docker/sql-scripts/init.sql
@@ -79,7 +79,7 @@ VALUES
 
 INSERT INTO consumer(id, username, created_at, updated_at)
 VALUES
-    ('e757b713-62e2-457e-8320-0e2fc4ac3a12', 'user1', current_timestamp, current_timestamp),
+    ('e757b713-62e2-457e-8320-0e2fc4ac3a12', 'user1', current_timestamp, current_timestamp);
 
 INSERT INTO jwt_credentials(id, key, secret, name, consumer_id, created_at)
 VALUES
